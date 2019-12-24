fdeea03070474fb41800ee355cdb7150a6b154df
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-24 18:10:23 -0500

    read json local file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/providers/menu_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,6 +13,7 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
+    print(menuProvider.options);
     return ListView(
       children: _createListItems(),
     );
