545de272f3ec9eb9f058a143a4565294f5a99d73
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-25 13:46:06 -0500

    container and animated container

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/animated_container.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/pages/avatar_page.dart';
@@ -6,9 +7,10 @@ import 'package:flutter_components/src/pages/card_page.dart';
 
 Map<String, WidgetBuilder> getApplicationsRoutes() {
   return <String, WidgetBuilder> {
-    '/'      : ( BuildContext context) => HomePage(),
-    'alert'  : ( BuildContext context) => AlertPage(),
-    'avatar' : ( BuildContext context) => AvatarPage(),
-    'card'   : ( BuildContext context) => CardPage(),
+    '/'                 : ( BuildContext context) => HomePage(),
+    'alert'             : ( BuildContext context) => AlertPage(),
+    'avatar'            : ( BuildContext context) => AvatarPage(),
+    'card'              : ( BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage()
   };
 }
